6284586767aa6f8040b11bb84918fb35ecddeb3f
Kewei Qu <kqu@brynmawr.edu>
2016-11-15 23:29:59 -0500

    imp terrain gen half way

--- hws/hw6/terrain.hpp
+++ hws/hw6/terrain.hpp
@@ -2,14 +2,17 @@
 #include <cmath>
 #include <cstring>
 #include <iostream>
+#include <ctime>
+#include <cstdlib>
 
 #define SIDE_LEN 4 + 1
 #define RECURSE 2
+#define RAND_RANGE 1024
 
 void initCorner(GLfloat height);
 void genTerrain(GLfloat height);
-void squareStep();
+void squareStep(int side_length);
 void diamondStep(int side_length);
 void fillSquareCenter(GLint top_left_x, GLint top_left_y, GLint side);
-void fillDiamondCenter(GLint center_x, GLint center_y, GLint side)
+void fillDiamondCenter(GLint center_x, GLint center_y, GLint side);
 void printSquare();
