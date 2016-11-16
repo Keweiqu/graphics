7c87cc23eb3d8a0328802e1a6c3f5b0631234ff6
Kewei Qu <kqu@brynmawr.edu>
2016-11-16 17:37:46 -0500

    tune terrain generation

--- hws/hw6/terrain.hpp
+++ hws/hw6/terrain.hpp
@@ -2,12 +2,15 @@
 #include <cmath>
 #include <cstring>
 #include <iostream>
+#include <fstream>
 #include <ctime>
 #include <cstdlib>
 
-#define SIDE_LEN 4 + 1
-#define RECURSE 2
-#define RAND_RANGE 1024
+using namespace std;
+
+#define RECURSE 7
+#define SIDE_LEN 129
+#define RAND_RANGE 4000
 
 void initCorner(GLfloat height);
 void genTerrain(GLfloat height);
@@ -16,3 +19,7 @@ void diamondStep(int side_length);
 void fillSquareCenter(GLint top_left_x, GLint top_left_y, GLint side);
 void fillDiamondCenter(GLint center_x, GLint center_y, GLint side);
 void printSquare();
+void genMeshOff();
+void genVertex(ofstream &mesh);
+void genFaces(ofstream &mesh);
+void removeContour();
